99377a4b6e3230904fb02e5a2dc64fadef37129c
Neerajv2000 <147934600+Neerajv2000@users.noreply.github.com>
2023-11-07 22:00:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,180 @@
+// let a = prompt ("enter input");
+
+// if(a=="html" || a=="HTML"  || a=="Html"){console.log(" we are learnig " + a);}
+
+// else if(a=="css" || a=="CSS"  || a=="Css"){console.log(" we are learnig " + a);}
+
+// else if(a=="java script" || a=="JAVA SCRIPT"  || a=="Java script"){console.log(" we are learnig " + a);}
+
+// else if(a=="python" || a=="PYTHON"  || a=="Python"){console.log(" we are learnig " + a);}
+
+// else{console.log("check your value ");}
+
+
+// let a = promp ("edho okati rayi ra babu");
+
+// switch(a){
+//   case 'na gunde':
+//   console.log("joke");
+//   break;
+//   case 'ne gunde':
+//   console.log("angry");
+//   break;
+//   case 'pakana gunde':
+//   console.log("mad");
+//   break;
+//   case 'venaka gunde':
+//   console.log("peace");
+//   break;
+// }
+
+//forloop
+//table
+// var a = prompt("enter the value") ;
+// for(var num=0; num<=10; num++){
+//   console.log(a+"*"+num+"="+num*a)
+// }
+
+
+
+// square elements
+// let a = 2;
+// for(var num=0; num<=10; num++){
+//   console.log(
+//     num**2
+//   )
+// }
+
+//while loop
+//  var num=0;
+// while(num<=10){
+//   console.log(num);
+//   num++;
+// }
+
+// let a = prompt("enter your color") ;
+// switch(a){
+//   case 'green' :
+//   console.log(" today monday is "+a);
+//   break;
+//   case 'blue' :
+//   console.log("monday is "+a);
+//   break;
+//   case 'red' :
+//   console.log("monday is "+a);
+//   break;
+//   case 'yello' :
+//   console.log("monday is "+a);
+//   break;
+//   case 'black' :
+//   console.log("monday is "+a);
+//   break;
+// }
+
+
+
+// 2/11/2023
+// loop
+// for(let i = 2; i<=15; i++){
+//   if(i%2==0){
+//     console.log(i+" is even numbes")
+//   }
+//   else(
+//     console.log(i + " is odd number")
+//   )
+// }
+
+//for(let i = 100; i>=1; i--)  reverse odder
+
+// for(let i = 1; i<=100; i++){
+//   if(i%5==0){
+//       console.log(i)
+//        }
+//       else if(i%7==0){
+//         console.log(i)
+//          }
+//          else if(i%11
+//           ==0){
+//           console.log(i)
+//            }
+
+// }
+
+// for(let i = 1; i<=100; i++){
+//   if(i%5==0 || i%7==0 || i%11==0 ){
+//     console.log(i)
+//   }
+// }
+
+
+// let a = 12;
+// switch(a){
+//   case 1:
+//   console.log("j");
+//   break;
+//   case 99:
+//   console.log("y");
+//   break;
+//   case 123:
+//   console.log("o");
+//   break;
+//   default: {
+//     console.log("NeErAj")
+//   }
+
+// }
+
+// let a = 1;
+// do{
+//   console.log(a);
+//   a++;
+// }
+// while(a<=10) //exit controll loop7
+
+
+
+//06/11/23
+
+//  var a=0;
+// while(a<=100){
+//   if(a%2==0){ 
+  
+//   console.log(a);
+ 
+// }
+// a++;
+
+// }
+
+
+// let a = 0;
+// do{
+//   console.log(a);
+//   a++;
+// }
+// while(a<=10);
+
+
+// let a = 5 ;
+//  if(a==2){
+//    for(i=1;i<=10;i++){
+//      console.log(a);
+//    }
+//  }
+//   else if (b==5){
+//     let b=1;
+//     while(b<=100){
+//       console/log(a);
+//     }    
+//   }
+
+// let i = 1;
+// while(i<=21){
+//   if(i%2!=0){
+//     console.log(i)
+//   }
+ 
+//   i++;
+// }
+
+
